20b5930a986970c1b9ac134a7d9003033ba366eb
SergeySharashkin <S_sharashkin@mail.ru>
2021-09-20 02:15:30 +0300

    Update task-01.js

---
+++ src/js/task-01.js
@@ -0,0 +1,13 @@
+const items = categories.querySelectorAll('.item')
+console.log('Number of categories: ', items.length)
+for (let item of items) {
+    const tittel = item.querySelector('h2');
+    const list = item.querySelector('ul');
+ const itemsEl = list.children;
+ console.log('Category:', tittel.textContent)
+ console.log('Elements: ', itemsEl.length)
+}
+
+
+// console.log(text)
+// console.log(items[0].textContent)
