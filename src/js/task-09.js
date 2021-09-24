b76abb98841479057fb7ed69d345b0218ff91b17
SergeySharashkin <S_sharashkin@mail.ru>
2021-09-25 02:39:30 +0300

    9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const buttonEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color')
+
+buttonEl.addEventListener('click', () => {
+ 
+  spanEl.textContent = getRandomHexColor(); 
+  bodyEl.style.backgroundColor = spanEl.textContent;
+});
+console.log(spanEl)
