7e312d750a995032ff75b334d6eac80702f076b8
SergeySharashkin <S_sharashkin@mail.ru>
2021-09-25 05:04:30 +0300

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,39 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// /ищу кнопки
+const buttonCreate = document.querySelector("[data-create]");
+const buttonDestroy = document.querySelector("[data-destroy]");
+// Ищу инпут
+const inputEl = document.querySelector("#controls input");
+// контейнер для боксов и стили для него
+const boxesEl = document.querySelector("#boxes");
+boxesEl.style.display = "flex";
+boxesEl.style.alignItems = "center";
+// достаю из инпута нужное количество боксов
+let number;
+inputEl.addEventListener("input", addNumber);
+function addNumber(event) {
+  console.log(event.currentTarget.value)
+ number = event.currentTarget.value
+ return
+}
+console.log(number);
+let amount = number;
+buttonCreate.addEventListener("click", createBoxes(amount));
+
+function createBoxes(amount) {
+  let newBoxes = [];
+  for (let i = 0; i < amount; i += 1) {
+    const newBox = document.createElement("div");
+    const sizeBox = 30 + i * 10;
+    newBox.style.width = `${sizeBox}px`;
+    newBox.style.height = `${sizeBox}px`;
+    newBox.style.backgroundColor = getRandomHexColor();
+    newBox.style.margin = "10px";
+    // console.log(newBox);
+    newBoxes.push(newBox);
+  }
+
+  boxesEl.append(...newBoxes);
+}
